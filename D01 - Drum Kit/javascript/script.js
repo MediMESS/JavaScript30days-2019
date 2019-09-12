71552fd5227bd7954bb9cd53dd8fa3c6b600acad
Mehdi Messarat <mehdimessarat@outlook.fr>
2019-09-12 12:12:11 +0100

    improving the code

--- D01 - Drum Kit/javascript/script.js
+++ D01 - Drum Kit/javascript/script.js
@@ -5,19 +5,21 @@ let audio = document.querySelectorAll("audio");
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-document.addEventListener("keydown",async function effectStyle (event) {
-  // this is in case we press many buttons at the same time.
-  for (effect of effects) {
-    effect.classList.remove("effects_style");
+function removeEffectStyle (event) {
+  if (event.propertyName == "transform") {
+    this.classList.remove("effects_style");
   }
+}
+
+document.addEventListener("keydown", event => {
+  // this is in case we press many buttons at the same time.
   for (effect of effects) {
     if(event.key.toUpperCase()==effect.children[1].textContent.toUpperCase() ) {
       effect.classList.add("effects_style");
-      duration = effect.children[0].duration * 1000;
       effect.children[0].play();
       effect.children[0].currentTime = 0;
-      await sleep(duration/2);
-      effect.classList.remove("effects_style");
+
+      effect.addEventListener("transitionend", removeEffectStyle);
       break;
     }
   }
