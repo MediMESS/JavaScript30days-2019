2c87faa43e9894673e043fc799987688be68706c
Mehdi Messarat <mehdimessarat@outlook.fr>
2019-09-12 08:51:49 +0100

    Finishing style

---
+++ D01 - Drum Kit/javascript/script.js
@@ -0,0 +1,46 @@
+let effects = document.querySelectorAll('.effect');
+let audio = document.querySelectorAll("audio");
+
+// sleep has to be implemented like this
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+document.addEventListener("keydown",async function effectStyle (event) {
+  // this is in case we press many buttons at the same time.
+  for (effect of effects) {
+    effect.classList.remove("effects_style");
+  }
+  for (effect of effects) {
+    if(event.key.toUpperCase()==effect.children[1].textContent.toUpperCase() ) {
+      effect.classList.add("effects_style");
+      duration = effect.children[0].duration * 1000;
+      effect.children[0].play();
+      effect.children[0].currentTime = 0;
+      await sleep(duration/2);
+      effect.classList.remove("effects_style");
+      break;
+    }
+  }
+});
+
+
+
+
+
+
+
+// if(event.key.toUpperCase()==effect.children[1].textContent.toUpperCase() ) {
+//   if (currEffect) {
+//     effect.classList.add("effects_style");
+//     effect.children[0].play();
+//     currEffect = effect;
+//     setTimeout( () => {currEffect.classList.remove("effects_style")}, 100);
+//   }
+//   else {
+//     effect.classList.add("effects_style");
+//     effect.children[0].play();
+//     currEffect = effect;
+//   }
+//   break;
+// }
+// }
