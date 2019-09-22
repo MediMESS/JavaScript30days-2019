995f96b4848dd4a1c40c9afada801236f31a28f8
Mehdi Messarat <mehdimessarat@outlook.fr>
2019-09-22 17:47:50 +0100

    useful very little cheatSheet arrays and objects

--- D14 - Reference vs copy/script.js
+++ D14 - Reference vs copy/script.js
@@ -1,1 +1,16 @@
 // as long as it's not an object then it'll be passed by value. otherwise by reference
+
+
+// copy array
+const team1 = players.slice();
+const team2 = [].concat(players);
+const team3 = [...players];
+const team4 = Array.from(players);
+
+// copy objects, shallow copy, only one level
+const captain1 = Object.assign({}, person);
+const captain2 = Object.assign({}, person, {number:99});
+const captain3 = {...person};
+
+// deep copy
+const captain4 = JSON.parse(JSON.stringify(person));
